e6baf767a10e0b91643920ba06a14a55c0a8fdf5
akiko_plays <blamemusic@gmail.com>
2021-12-16 10:45:05 +0100

    minor refactoring and cleanups

--- agt_AABB.cpp
+++ agt_AABB.cpp
@@ -1,13 +1,11 @@
-#include "agt_stdafx.h"
 #include "agt_AABB.h"
+#include "agt_stdafx.h"
 #include "agt_utils.h"
 
-using namespace agt3d;
-
-AABB::AABB() : min({ 0,0,0 }), max({ 0,0,0 })
+namespace agt3d
 {
-  return;
-}
+
+AABB::AABB() : min({0, 0, 0}), max({0, 0, 0}) {}
 
 void AABB::calculateFromPoints(const glm::vec3* verts, const uint32_t numVerts)
 {
@@ -18,19 +16,27 @@ void AABB::calculateFromPoints(const glm::vec3* verts, const uint32_t numVerts)
   max = verts[0];
 
   for (i = 1; i < numVerts; i++) {
-    if (verts[i].x < min.x)
-      min.x = verts[i].x;
-    if (verts[i].y < min.y)
-      min.y = verts[i].y;
-    if (verts[i].z < min.z)
-      min.z = verts[i].z;
-    if (verts[i].x > max.x)
-      max.x = verts[i].x;
-    if (verts[i].y > max.y)
-      max.y = verts[i].y;
-    if (verts[i].z > max.z)
-      max.z = verts[i].z;
+    if (verts[i].x < min.x) min.x = verts[i].x;
+    if (verts[i].y < min.y) min.y = verts[i].y;
+    if (verts[i].z < min.z) min.z = verts[i].z;
+    if (verts[i].x > max.x) max.x = verts[i].x;
+    if (verts[i].y > max.y) max.y = verts[i].y;
+    if (verts[i].z > max.z) max.z = verts[i].z;
   }
-
-  return;
 }
+
+BoundingSphere& BoundingSphere::operator+(const BoundingSphere& a) noexcept
+{
+  center = a.center;
+  radius = a.radius;
+  return *this;
+}
+
+BoundingSphere& BoundingSphere::operator+=(const BoundingSphere& rhs) noexcept
+{
+  center += rhs.center;
+  radius += rhs.radius;
+  return *this;
+}
+
+}  // namespace agt3d
--- agt_AABB.h
+++ agt_AABB.h
@@ -1,34 +1,24 @@
 #pragma once
 
-namespace agt3d {
+#include <glm/glm/glm.hpp>
 
-  class AABB
-  {
-  public:
-    AABB();
-    void calculateFromPoints(const glm::vec3* verts, const uint32_t numVerts);
-    glm::vec3 min;
-    glm::vec3 max;
-  };
+namespace agt3d
+{
 
-  struct BoundingSphere
-  {
-    glm::vec3 center;
-    float radius;
+struct AABB {
+  AABB();
+  void calculateFromPoints(const glm::vec3* verts, const uint32_t numVerts);
+  glm::vec3 min;
+  glm::vec3 max;
+};
 
-    BoundingSphere& operator + (const BoundingSphere& a)
-    {
-      center = a.center;
-      radius = a.radius;
-      return *this;
-    }
+struct BoundingSphere {
+  glm::vec3 center;
+  float radius;
 
-    BoundingSphere& operator+=(const BoundingSphere& rhs)
-    {
-      center += rhs.center;
-      radius += rhs.radius;
-      return *this;
-    }
-  };
+  BoundingSphere& operator+(const BoundingSphere& a) noexcept;
 
-}
+  BoundingSphere& operator+=(const BoundingSphere& rhs) noexcept;
+};
+
+}  // namespace agt3d
